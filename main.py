140acc202cb133de89aff6137af3c1a5060bc2ad
Brandon Yáñez Ponce <br.yanezp@duocuc.cl>
2024-06-26 13:07:40 -0400

    Orden

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import random
 import math
 import statistics
 
+# Funciones
+# Saldos randomizados entre 100.000 y 2.000.000
 def saldoRandom():
     saldos = random.randint(100000, 2000000)
     return saldos
 
+# Asignación de saldos a usuarios
+def asignacionesSaldos():
+    for i in usuarios:
+        usuarios[i] = saldoRandom()
+        return usuarios[i]
+
+
+# Diccionario de usuarios
 usuarios = {
     "Juan": 0,
     "Leo": 0,
     "Maria": 0,
     "Jorge": 0,
     "Brandon": 0,
-    "Ana": 0
+    "Ana": 0,
+    "Pablo": 0,
+    "Ron": 0,
+    "Luis": 0,
+    "Jose": 0
 }
 
-def asignacionesSaldos():
-    for i in usuarios:
-        usuarios[i] = saldoRandom()
-        return usuarios[i]
